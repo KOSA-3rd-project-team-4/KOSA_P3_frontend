ccfa13364b361ef3a8809c3c6273c4e3f55f737c
Beomky <nobleman1214@naver.com>
2024-08-22 17:32:54 +0900

    Create : mainpage layouts
    header/middle : 80%
    footer: not yet

---
+++ src/components/KBC/middle/MainLeftCompo.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="mainTopLeft">
+  
+    <ul id="PrimaryMenu" class="main-primary"> 
+        
+        <li id="PrimaryLocal">
+            <div class="main-primary-title">
+                지역별 알바</div>
+            <div class="primary-content-align">
+                <div class="primary-local">
+                    <a class="primary-local-item">서울</a>
+                    <a class="primary-local-item">경기</a>
+                    <a class="primary-local-item">인천</a>
+                    <a class="primary-local-item">부산</a>
+                    <a class="primary-local-item">대구</a>
+                    <a class="primary-local-item">대전</a>
+                    <a class="primary-local-item">경남</a>
+                    <a class="primary-local-item">전남</a>
+                    <a class="primary-local-item">충남</a>
+                    <a class="primary-local-item">광주</a>
+                    <a class="primary-local-item">울산</a>
+                    <a class="primary-local-item">경북</a>
+                    <a class="primary-local-item">전북</a>
+                    <a class="primary-local-item">충북</a>
+                    <a class="primary-local-item">강원</a>
+                    <a class="primary-local-item">제주</a>
+                    <a class="primary-local-item">세종</a>
+                </div>
+                <ul class="primary-local-category">
+                    <li class="primary-localcategory-item">
+                        <a>역세권</a>
+                    </li>
+                    <li class="primary-localcategory-item">
+                        <a>대학가</a>
+                    </li>
+                </ul>  
+            </div>  
+        </li>
+        <li id="PrimaryTheme">
+            <div class="main-primary-title">테마별 알바</div>
+            <ul class="main-primary-contents primary-category">
+                <li class="primary-category-item">
+                    <a>식당</a>
+                </li>
+                <li class="primary-category-item">
+                    <a>편의점</a>
+                </li>
+                <li class="primary-category-item">
+                    <a>운동센터</a>
+                </li>
+                <li class="primary-category-item">
+                    <a>공공기관</a>
+                </li>
+            </ul>
+        </li>
+        <li id="PrimaryPrefer">
+            <div class="main-primary-title">대상별 알바</div>
+            <ul class="main-primary-contents primary-category">
+                <li class="primary-prefer-category-item">
+                    <a>중장년</a>
+                </li>
+                <li class="primary-prefer-category-item">
+                    <a>대학생</a>
+                </li>
+                <li class="primary-prefer-category-item">
+                    <a>청소년</a>
+                </li>
+                <li class="primary-prefer-category-item">
+                    <a>주부</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  @import url('/src/assets/middletop.css');
+</style>
